32fa6e1c397802d2c1c3f04ad93dacc250deee5a
QuanNH97 <quannh220597@gmail.com>
2020-08-12 13:55:44 +0700

    Initial Commit

---
+++ demoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoLayout
+//
+//  Created by Quan Nguyen on 8/12/20.
+//  Copyright © 2020 quannh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
